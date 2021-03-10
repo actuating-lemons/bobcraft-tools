5b74139a61b4955e67fd482ac770270f211cbd18
ACBob <acbobcat@protonmail.com>
2021-03-10 18:45:19 +0000

    all 100 textures from minecraft we share

--- packconv.py
+++ packconv.py
@@ -15,6 +15,129 @@ texture_indices = {
 	"dirt.png": [2,0],
 	"grass_block_side.png": [3,0],
 	"planks.png": [4,0],
+	"bricks.png": [7,0],
+	"cobweb.png": [11,0],
+	"rose.png": [12,0],
+	"oak_sapling.png": [15,0],
+
+	"cobblestone.png": [0,1],
+	"bedrock.png": [1,1],
+	"sand.png": [2,1],
+	"gravel.png": [3,1],
+	"log_side.png": [4,1],
+	"log_top.png": [5,1],
+	
+	"iron_block.png": [6,1],
+	"gold_block.png": [7,1],
+	"diamond_block.png": [8,1],
+
+	"chest_top.png": [9,1],
+	"chest_side.png": [10,1],
+	"chest_front.png": [11,1],
+
+	"gold_ore.png": [0,2],
+	"iron_ore.png": [1,2],
+	"coal_ore.png": [2,2],
+	"bookshelf.png": [3,2],
+
+	"obsidian.png": [5,2],
+	"grass_block_side_overlay.png": [6,2],
+	"grass.png": [7,2],
+	"grass_block_top_overlay.png": [8,2],
+
+	"crafting_table_top.png": [11,2],
+	"furnace_front.png": [12,2],
+	"furnace_side.png": [13,2],
+
+	"sponge.png": [0,3],
+	"glass.png": [1,3],
+	"diamond_ore.png": [2,3],
+	"greendust_ore_active.png": [3,3],
+	"leaves.png": [4,3],
+
+	"stone_brick.png": [6,3],
+	"deadbush.png": [7,3],
+	"crafting_table_side.png": [11,3],
+	# 12, 3 is also a valid choice, but we only use one texture
+	"furnace_front_active.png": [13,3],
+	"furnace_top.png": [14,3],
+
+	"wool_white.png": [0,4],
+	"snow.png": [2,4],
+	"grass_block_top_snowy.png": [2,4], # duplicate texture! eek!
+	"ice.png": [3,4],
+	"grass_block_side_snow.png": [4,4],
+	"cactus_top.png": [5,4],
+	"cactus_side.png": [6,4],
+	"clay.png": [8,4],
+	"sugarcane.png": [9,4],
+
+	"torch.png": [0,5],
+	"door_top.png": [1,5],
+	"ladder.png": [3,5],
+	"trapdoor.png": [4,5],
+	"iron_bars.png": [5,5],
+	"farmland_wet.png": [6,5],
+	"farmland.png": [7,5],
+	
+	# wheat!
+	"wheat_plant_0.png": [8, 5],
+	"wheat_plant_1.png": [9, 5],
+	"wheat_plant_2.png": [10,5],
+	"wheat_plant_3.png": [11,5],
+	"wheat_plant_4.png": [12,5],
+	"wheat_plant_5.png": [13,5],
+	"wheat_plant_6.png": [14,5],
+	"wheat_plant_7.png": [15,5],
+
+	"door_bottom.png": [1,6],
+	"pumpkin_top.png": [6,6],
+
+	"hellstone.png": [7,6],
+	"hellsand.png": [8,6],
+	"vibrantstone.png": [9,6],
+	
+	"wool_black.png": [1,7],
+	"wool_dark_grey.png": [2,7],
+
+	"pumpkin_side.png": [6,7],
+	"jackolantern_face.png": [7,7],
+	"jackolantern_face_lit.png": [8,7],
+
+	"wool_red.png": [1,8],
+	"wool_pink.png": [2,8],
+
+	"bed_top_bottom.png": [6,8],
+	"bed_top_top.png": [7,8],
+	"melon_side.png": [8,8],
+	"melon_top.png": [9,8],
+
+	"wool_green.png": [1,9],
+	"wool_lime.png": [2,9],
+
+	"bed_end_bottom.png": [5,9],
+	"bed_side_bottom.png": [6,9], # nice
+	"bed_side_top.png": [7,9],
+	"bed_end_top.png": [8,9],
+
+	"wool_brown.png": [1,10],
+	"wool_yellow.png": [2,10],
+	
+	"sandstone_top.png": [0,11],
+	"wool_blue.png": [1,11],
+	"wool_picton.png": [2,11],
+
+	"sandstone_side.png": [0,12],
+	"wool_purple.png": [1,12],
+	"wool_magenta.png": [2,12],
+	
+	"sandstone_bottom.png": [0,13],
+	"wool_cyan.png": [1,13],
+	"wool_orange.png": [2,13],
+	
+	"chiseled_stone_brick.png": [5,13],
+
+	"wool_grey.png": [1,14],
 }
 
 if __name__ == "__main__":
