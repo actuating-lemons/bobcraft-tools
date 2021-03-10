81b67cdcc520d3bdb30948597119fdf17bb6d33c
ACBob <acbobcat@protonmail.com>
2021-03-10 17:52:28 +0000

    simple exporting

--- packconv.py
+++ packconv.py
@@ -6,6 +6,14 @@ import zipfile
 from PIL import Image
 import os
 import sys
+import shutil
+
+texture_indices = {
+	"stone.png": [1,0],
+	"dirt.png": [2,0],
+	"grass_block_side.png": [3,0],
+	"planks.png": [4,0],
+}
 
 if __name__ == "__main__":
 
@@ -25,7 +33,28 @@ if __name__ == "__main__":
 	if not "terrain.png" in jar.namelist():
 		print("minecraft.jar does not have a terrain.png file. What version is it from? This tool is built for 1.2.5!")
 		sys.exit(-1)
+
+	if not os.path.exists("bobcraft-minecraft-texturepack"):
+		print("Output directory doesn't exist, creating...")
+	else:
+		print("Output directory exists, cleaning...")
+		shutil.rmtree("bobcraft-minecraft-texturepack")
+
+	os.mkdir("bobcraft-minecraft-texturepack")
 	
 	terrainpng = jar.open("terrain.png")
 	terrainpng = Image.open(terrainpng)
-	terrainpng.show()
+
+	for tex in texture_indices:
+		indice = texture_indices[tex]
+
+		indice[0] = indice[0] * 16
+		indice[1] = indice[1] * 16
+
+		texture = terrainpng.crop((indice[0], indice[1],
+								  indice[0]+16, indice[1]+16))
+		
+
+		texture.save(
+			os.path.join("bobcraft-minecraft-texturepack", tex)
+			)
